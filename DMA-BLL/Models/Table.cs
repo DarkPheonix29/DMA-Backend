b28ee5480fe8f9d5377417a17117824656b93ed5
DarkPheonix29 <kealanthal29@gmail.com>
2025-04-11 11:30:01 +0200

    table model update

--- DMA-BLL/Models/Table.cs
+++ DMA-BLL/Models/Table.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DMA_BLL.Models
 {
 	public class Table
 	{
+		[Key]
 		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; } = string.Empty;
 		public string UniqueCode { get; set; } = Guid.NewGuid().ToString();
 	}
